d4b1bc5053316b19566f7f4db3bd860b9ade175f
Paul Bennett <paul@prajnainc.com>
2016-12-15 20:32:39 -0500

    Updated to clojure 1.9-alpha14

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.9.0-alpha11"]
+                 [org.clojure/clojure "1.9.0-alpha14"]
                  [com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
                  [cheshire "5.6.3"]
